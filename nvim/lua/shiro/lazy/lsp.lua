7cbe473bcc3b1ddd2c7d8c4f71a335ba69e6fd94
mushcatshiro <yingjieho@hotmail.com>
2025-09-14 23:55:06 +0800

    adding nvim cfg

---
+++ nvim/lua/shiro/lazy/lsp.lua
@@ -0,0 +1,66 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        -- Automatically install LSPs and formatters
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        -- This is where all the LSP configuration goes
+        require("conform").setup({
+            formatters_by_ft = {
+            }
+        })
+
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities())
+
+        -- Set up mason so it can manage LSP servers
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "gopls",
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                format = {
+                                    enable = true,
+                                    -- Put format options here
+                                    -- NOTE: the value should be STRING!!
+                                    defaultConfig = {
+                                        indent_style = "space",
+                                        indent_size = "2",
+                                    }
+                                },
+                            }
+                        }
+                    }
+                end,
+        }
+    })
+    vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end
+}
