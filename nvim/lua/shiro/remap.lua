9974ab374c36ab635a51d31c9b6912816e8893c2
mushcatshiro <yingjieho@hotmail.com>
2025-12-05 19:02:49 +0800

    double quote

--- nvim/lua/shiro/remap.lua
+++ nvim/lua/shiro/remap.lua
@@ -1,3 +1,3 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('n', '<leader>l', function() vim.o.list = not vim.o.list end)
+vim.keymap.set("n", "<leader>l", function() vim.o.list = not vim.o.list end)
